ed71c345dc5a60558981ff4b7b5a141ea4225b89
Devin Pastoor <devin.pastoor@gmail.com>
2022-10-22 23:16:17 -0400

    chore: minor doc tweak

--- example/main.py
+++ example/main.py
@@ -7,16 +7,11 @@ app = FastAPI()
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+# lets log to console with a decent format
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
-# create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
 ch.setFormatter(formatter)
-
-# add ch to logger
 log.addHandler(ch)
 
 log.info("starting up")
@@ -28,10 +23,10 @@ async def root():
 def fsync():
     request_id = uuid.uuid4()
     thread_id = threading.get_ident()
-    log.debug("%s - start of fast_sync",request_id)
+    log.debug("%s - start of sync on thread %s",request_id, thread_id)
     start_time = time.time()
     time.sleep(2)
-    log.debug("%s about to return fast_sync", request_id)
+    log.debug("%s about to return sync on thread %s", request_id, thread_id)
     return {
         "start_time": start_time,
         "send_time": time.time(),
@@ -43,10 +38,10 @@ def fsync():
 async def fasync():
     request_id = uuid.uuid4()
     thread_id = threading.get_ident()
-    log.debug("%s - start of fast_async",request_id)
+    log.debug("%s - start of async on thread %s",request_id, thread_id)
     start_time = time.time()
     time.sleep(2)
-    log.debug("%s about to return fast_async", request_id)
+    log.debug("%s about to return async on thread %s", request_id, thread_id)
     return {
         "start_time": start_time,
         "send_time": time.time(),
